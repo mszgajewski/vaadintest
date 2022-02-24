b60f504dcc5c49e1481eae7176d31e01183ea1cb
mszgajewski <mszgajewski@gmail.com>
2022-02-24 21:23:00 +0100

    Initial Commit

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,9 +1,13 @@
 package com.example.application.views.list;
 
 import com.example.application.data.entity.Contact;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -18,11 +22,33 @@ public class ListView extends VerticalLayout {
         setSizeFull();
 
         configureGrid();
+
+        add(
+                getToolbar(),
+                grid
+        );
+    }
+
+    private Component getToolbar() {
+        filterText.setPlaceholder("filter by name.");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        Button addContactButton = new Button("add contact");
+
+        HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+        toolbar.addClassName("toolbar");
+        return toolbar;
     }
 
     private void configureGrid() {
         grid.addClassName("contact-grid");
         grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email");
+        grid.addColumn(contact -> contact.getStatus().getName()).setHeader("Status");
+        grid.addColumn(contact -> contact.getCompany().getName()).setHeader("Company");
+        grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
     }
 
 }
